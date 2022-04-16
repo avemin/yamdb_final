c2593cbaa43325c85681a4f1de2833620bc4f9c2
avemin <av-emin@mail.ru>
2022-04-16 15:27:19 +0700

    final v1.5

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,8 +1,6 @@
-from .models import User, Comments, Review, Category, Genre, Title
-
 from django.conf import settings
 from django.contrib import admin
-
+from .models import User, Comments, Review, Category, Genre, Title
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
